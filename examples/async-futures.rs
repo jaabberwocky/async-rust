2bba65fb5bd399ea01bd976cd26ad76e5deecce2
tobias <tobias@tobias-leong.dev>
2023-12-18 22:54:16 +0800

    Add async-futures

---
+++ examples/async-futures.rs
@@ -0,0 +1,57 @@
+use futures::future::Future;
+use std::thread;
+
+fn foo2() -> impl Future<Output = i32> {
+    async {
+        println!("foo2");
+        42
+    }
+}
+
+trait SimpleFuture {
+    type Output;
+    fn poll(&mut self, wake: fn()) -> Poll<Self::Output>;
+}
+
+enum Poll<T> {
+    Ready(T),
+    Pending,
+}
+
+pub struct SimpleSum {
+    sum: i32,
+    limit: i32,
+}
+
+impl SimpleFuture for SimpleSum {
+    type Output = i32;
+
+    fn poll(&mut self, wake: fn()) -> Poll<Self::Output> {
+        if self.sum < self.limit {
+            self.sum += 1;
+            Poll::Pending
+        } else {
+            wake();
+            Poll::Ready(self.sum)
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let fut = foo2().await;
+    println!("main");
+    println!("fut: {}", fut);
+
+    let mut ss: SimpleSum = SimpleSum { sum: 0, limit: 10 };
+    while match ss.poll(|| println!("wake")) {
+        Poll::Ready(val) => {
+            println!("val: {}", val);
+            false
+        }
+        Poll::Pending => {
+            println!("pending");
+            true
+        }
+    } {}
+}
